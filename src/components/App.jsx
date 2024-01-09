5305fdddd479fed986443b37ec7e748e73cee2f7
BoikoAlona <a.semikhova@gmail.com>
2024-01-10 00:56:49 +0200

    hw02 p1 step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleClick = (event) => {
+    this.setState((prevState) => {
+      
+      const buttonId = event.target.id;
+
+      return {
+        [buttonId]: prevState[buttonId] + 1,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button id="good" onClick={this.handleClick}>Good</button>
+        <button id="neutral" onClick={this.handleClick}>Neutral</button>
+        <button id="bad" onClick={this.handleClick}>Bad</button>
+        <h1>Statistics</h1>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    )
+  }
+  
+}
